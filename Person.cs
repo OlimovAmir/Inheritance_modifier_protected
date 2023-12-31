55eae460a0daef57942420c87e3877e5db99d072
olimov Amir <olimov.amir@mail.ru>
2023-12-31 11:41:08 +0300

    Добавьте файлы проекта.

---
+++ Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_modifier_protected
+{
+    internal class Person
+    {
+        public Guid Id { get;} = Guid.NewGuid();
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public Person(string name, string lastName, DateTime birthday)
+        {
+            Name = name;
+            LastName = lastName;
+            Birthday = birthday;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} {Name} {LastName} {Birthday.ToShortDateString()}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using Inheritance_modifier_protected;
+using System;
+using System.Collections.Generic;
+
+
+
+class Program
+{
+    class A
+    {
+        public int publicFiled;
+        private int privateFiled;
+        protected int protectedFiled;
+
+
+        public A()
+        {
+            Console.WriteLine(privateFiled);
+            Console.WriteLine(publicFiled);
+            Console.WriteLine(protectedFiled);
+        }
+    }
+
+    
+
+    static void Main(string[] args)
+    {
+
+        A a = new A();
+        string filePath = "file.txt";
+
+       
+    }
+
+
+
+
+    public void WriteToFile(List<Person> people ,string filePath)
+    {
+        try
+        {
+            using(StreamWriter sw = new StreamWriter(filePath))
+            {
+                foreach(Person person in people)
+                {
+                    sw.WriteLine($"{person.Id} {person.Name} {person.LastName} {person.Birthday.ToShortDateString()}");
+                }
+                Console.WriteLine($" {filePath}");
+            }
+        }catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
